ab6b3224dcbfd3d050141e38846b5867903ec2d3
Mrlizard <mrlizardtaco@gmail.com>
2025-08-27 13:32:35 +1000

    Add files via upload

---
+++ Key1.bat
@@ -0,0 +1,6 @@
+@echo off
+set "folder=C:\Windows\System32\system.start"
+cd %folder%
+mkdir "%folder%"
+echo wadh6wa09ENDa8e7897wq489wne7bfen40QE86ec7b8woqbq > C:\Windows\System32\system.start\sys.dll
+pause
